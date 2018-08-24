d95f9fe2d2f13e2f11efddcf281c33d8199689a2
Felix Pfister <mail@felix-pfister.de>
2018-08-24 10:52:22 +0200

    initial commit

---
+++ src/FondOfSpryker/Zed/Brand/BrandApiDependencyProvider.php
@@ -0,0 +1,9 @@
+<?php
+namespace FondOfSpryker\Zed\ProductApi;
+
+use Spryker\Zed\Kernel\Container;
+use Spryker\Zed\ProductApi\ProductApiDependencyProvider as BaseProductApiDependencyProvider;
+
+class BrandApiDependencyProvider extends BaseProductApiDependencyProvider
+{
+}
